7d56d35c11fcf6d210648d9f74a1f697780465f3
hanaaden <hanaadamabdi@gmail.com>
2025-07-07 21:16:13 +0300

    Day 1 almost done

---
+++ Day-1/index.js
@@ -0,0 +1,85 @@
+// let is block scope
+// Hoisting: Hoisted but not initialized, so accessing it before declaration results in a ReferenceError.
+// paractices: for variables that need to be reassigned.
+
+// ========================================
+
+// var is function scope
+// Hoisting: var are hoisted to the top of their scope and initialized as undefined.
+// paractice  Generally avoided in modern JavaScript
+
+// ========================================
+
+ // const is block scoped
+// Hoisting: Hoisted but not initialized, so accessing it before declaration results in a ReferenceErro
+// for variables that should not be reassigned.
+
+// ========================================
+
+// scope refers how varaibles,objects and functions are accesible 
+// = function scope is varaible declered within the function\
+// they are only accesible within function
+
+// ========================================
+
+function myfun(){
+    var local = 100
+    console.log("the local variable is " + local)
+}
+
+// if we console console.log(local) now here it will give error
+myfun()
+
+// = block scope Variables declared with let or const
+//  inside a block like within {}  are only accessible within that block.
+{
+    let block = "I am block"
+    console.log(block)
+}
+// if we console console.log(block) now here it will give error
+
+let num1 = 10;
+let num2 = 5;
+
+var sum = num1 + num2;
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+var remainder = num1 % num2;
+console.log(sum)
+console.log(difference)
+console.log(product)
+console.log(quotient)
+console.log(remainder)
+
+if(num1 === num2){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+if(num1>0 && num2>0){
+    console.log('yeah greater')
+}else{
+    console.log('no greater')
+}
+
+var grade = "Enter your Grade A B C D or F"
+
+switch(grade){
+    case 'A':
+        alert("Excellent")
+        break
+    case 'B':
+        alert("Very good")
+        break
+    case 'C':
+        alert("good")
+        break
+    case 'D':
+        alert("you passed")
+        break
+    case 'F':
+        alert("Failed")
+        break
+}
